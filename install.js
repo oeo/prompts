f1feb750d7ebdf377c52d32dbccc8d3743af7981
boobie <boobie@riseup.net>
2024-12-01 18:34:58 -0500

    test: verify hook installation

--- install.js
+++ install.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const { readdirSync } = require('fs');
+const path = require('path');
 
 try {
   // install dependencies
@@ -17,7 +19,13 @@ try {
   
   // install git hooks
   console.log('Installing git hooks...');
-  execSync('cp git-hooks/* .git/hooks/', { stdio: 'inherit' });
+  const hookFiles = readdirSync('git-hooks');
+  for (const file of hookFiles) {
+    if (file.endsWith('.example')) {
+      const hookName = file.replace('.example', '');
+      execSync(`cp git-hooks/${file} .git/hooks/${hookName}`, { stdio: 'inherit' });
+    }
+  }
   execSync('chmod +x .git/hooks/*', { stdio: 'inherit' });
   
   console.log('Installation complete!');
